d4f67052e8ddf42c3821d99f8cf70321e4a973ee
WON <won431236@gmail.com>
2025-06-05 21:31:29 +0900

    ✨ 로그인, 회원가입 구현 (상태 부분 제외)

--- src/routes/pages/Login.tsx
+++ src/routes/pages/Login.tsx
@@ -1,6 +1,86 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import supabase from "../../utils/supabase";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+
+  const signInWithGoogle = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    try {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: "google",
+      });
+      if (error) {
+        console.log(error);
+        alert("로그인이 정상적으로 완료되지 않았습니다.");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("로그인이 정상적으로 완료되지 않았습니다.");
+    }
+  };
+  const signInWithFacebook = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    try {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: "facebook",
+      });
+      if (error) {
+        console.log(error);
+        alert("로그인이 정상적으로 완료되지 않았습니다.");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("로그인이 정상적으로 완료되지 않았습니다.");
+    }
+  };
+  const signInWithGithub = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    try {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider: "github",
+      });
+      if (error) {
+        console.log(error);
+        alert("로그인이 정상적으로 완료되지 않았습니다.");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("로그인이 정상적으로 완료되지 않았습니다.");
+    }
+  };
+  const loginHandler = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: emailInput,
+        password: passwordInput,
+      });
+      console.log([emailInput, passwordInput]);
+      if (error) {
+        console.log(error);
+        alert("로그인이 정상적으로 완료되지 않았습니다.");
+      } else if (data) {
+        alert("로그인되었습니다.");
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("로그인이 정상적으로 완료되지 않았습니다.");
+    }
+  };
+
   return (
     <div className="h-[88vh] w-full flex">
       <div className="w-1/2 flex items-center justify-center"></div>
@@ -15,6 +95,8 @@ export default function Login() {
                   type="email"
                   className="login-input"
                   placeholder="이메일을 입력해주세요"
+                  value={emailInput}
+                  onChange={(e) => setEmailInput(e.target.value)}
                 />
               </div>
               <div className="flex flex-col gap-1">
@@ -23,9 +105,15 @@ export default function Login() {
                   type="password"
                   className="login-input"
                   placeholder="비밀번호를 입력해주세요"
+                  value={passwordInput}
+                  onChange={(e) => setPasswordInput(e.target.value)}
                 />
               </div>
-              <button className="login-btn font-yapari text-[12px] mt-3">
+              <button
+                className="login-btn font-yapari mt-4"
+                disabled={emailInput.length === 0 || passwordInput.length === 0}
+                onClick={loginHandler}
+              >
                 LOGIN
               </button>
             </div>
@@ -34,8 +122,8 @@ export default function Login() {
               <span className="text-sm">OR</span>
               <hr className="w-full" />
             </div>
-            <div className="w-full flex flex-col gap-3">
-              <button className="login-btn text-white">
+            <div className="w-full flex flex-col gap-2.5">
+              <button className="social-login-btn" onClick={signInWithGoogle}>
                 <img
                   src="https://static.wikia.nocookie.net/logopedia/images/2/2b/Google_icon-Sep15.svg"
                   alt="google"
@@ -43,7 +131,7 @@ export default function Login() {
                 />
                 Sign in with Google
               </button>
-              <button className="login-btn text-white">
+              <button className="social-login-btn" onClick={signInWithFacebook}>
                 <img
                   src="https://static.wikia.nocookie.net/logopedia/images/8/82/Facebook_icon_2023.svg"
                   alt="facebook"
@@ -51,8 +139,9 @@ export default function Login() {
                 />
                 Sign in with Facebook
               </button>
-              <button className="login-btn text-white">
+              <button className="social-login-btn" onClick={signInWithGithub}>
                 <img
+                  // src="https://static.wikia.nocookie.net/logopedia/images/8/8f/25231.svg"
                   src="https://static.wikia.nocookie.net/logopedia/images/0/0e/Github_Icon_White.svg"
                   alt="github"
                   className="size-5"
@@ -61,10 +150,13 @@ export default function Login() {
               </button>
             </div>
             <div className="flex w-full justify-between">
-              <button className="text-sm cursor-pointer">
+              <button className="text-sm cursor-pointer hover:text-[var(--primary-300)]">
                 Forgot Your Password?
               </button>
-              <Link to="/signup" className="text-sm cursor-pointer">
+              <Link
+                to="/signup"
+                className="text-sm cursor-pointer hover:text-[var(--primary-300)]"
+              >
                 Sign Up
               </Link>
             </div>
