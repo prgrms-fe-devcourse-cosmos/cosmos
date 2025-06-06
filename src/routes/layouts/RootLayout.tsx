7d8d0139040a717d24d781c7ff1524f2c874ada8
hxezu <hailey_apple@icloud.com>
2025-06-07 00:31:37 +0900

    💄 RootLayout 배경 이미지 추가

--- src/routes/layouts/RootLayout.tsx
+++ src/routes/layouts/RootLayout.tsx
@@ -6,7 +6,7 @@ import Footer from "../../components/common/Footer";
 export default function RootLayout() {
   return (
     <>
-      <div className="flex flex-col min-h-screen bg-[#0D1117] text-[#c9d1d9] w-full">
+      <div className="flex flex-col min-h-screen bg-[url('/images/galaxy_bg.png')] bg-cover bg-center bg-no-repeat bg-fixed text-[#c9d1d9] w-full">
         <Header />
 
         <main className="flex-grow w-full flex justify-center items-center">
