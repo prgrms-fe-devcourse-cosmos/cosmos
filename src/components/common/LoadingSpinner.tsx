8d86b7d1000b0c6807e48244b2e2a0ab8d37e87c
hxezu <hailey_apple@icloud.com>
2025-06-09 10:37:28 +0900

    💄 loading spinner 추가

---
+++ src/components/common/LoadingSpinner.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+export default function LoadingSpinner() {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <DotLottieReact
+        src="https://lottie.host/b994bb3f-556d-41c4-bb8c-9b0d00752c21/1tS2fFMhvw.lottie"
+        loop
+        autoplay
+        style={{ width: "180px", height: "100px" }}
+      />
+    </div>
+  );
+}
