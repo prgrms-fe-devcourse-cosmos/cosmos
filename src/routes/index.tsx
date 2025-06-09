4a4d4d11d32db885b3bdec30f824a273b4cac29c
TYss00 <rlaxodus203@naver.com>
2025-06-09 15:20:35 +0900

    라우터의 로딩 추가 및 로딩 위치 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,56 +2,56 @@ import {
   createBrowserRouter,
   Navigate,
   RouterProvider,
-} from "react-router-dom";
-import NotFound from "./pages/NotFound";
-import Home from "./pages/Home";
-import Default from "./layouts/RootLayout";
-import Daily from "./pages/Daily";
-import Lounge from "./pages/Lounge";
-import Lab from "./pages/lab/Lab";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import LabQuiz from "./pages/lab/quiz/LabQuiz";
-import LabPuzzle from "./pages/lab/puzzle/LabPuzzle";
-import { DailyLoader } from "./loader/dallyspace.loader";
-import PuzzleConfig from "./pages/lab/puzzle/PuzzleConfig";
-import PuzzleScreen from "./pages/lab/puzzle/PuzzleScreen";
-
+} from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import Default from './layouts/RootLayout';
+import Daily from './pages/Daily';
+import Lounge from './pages/Lounge';
+import Lab from './pages/lab/Lab';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import LabQuiz from './pages/lab/quiz/LabQuiz';
+import LabPuzzle from './pages/lab/puzzle/LabPuzzle';
+import { DailyLoader } from './loader/dallyspace.loader';
+import PuzzleConfig from './pages/lab/puzzle/PuzzleConfig';
+import PuzzleScreen from './pages/lab/puzzle/PuzzleScreen';
+import LoadingSpinner from '../components/common/LoadingSpinner';
 
 const router = createBrowserRouter([
   {
     element: <Default />,
-    hydrateFallbackElement: <h1>Loading...</h1>,
+    hydrateFallbackElement: <LoadingSpinner />,
     errorElement: <div>데이터를 불러오는 데 실패했습니다.</div>,
     children: [
-      { path: "/", element: <Home /> },
-      { path: "/login", element: <Login /> },
+      { path: '/', element: <Home /> },
+      { path: '/login', element: <Login /> },
       {
-        path: "/daily",
+        path: '/daily',
         loader: DailyLoader,
         element: <Daily />,
       },
-      { path: "/lounge", element: <Lounge /> },
-      { path: "/signup", element: <Signup /> },
+      { path: '/lounge', element: <Lounge /> },
+      { path: '/signup', element: <Signup /> },
       {
-        path: "/lab",
+        path: '/lab',
         element: <Lab />,
         children: [
-          { path: "quiz", element: <LabQuiz /> },
+          { path: 'quiz', element: <LabQuiz /> },
           {
-            path: "puzzle",
+            path: 'puzzle',
             element: <LabPuzzle />,
             children: [
               { index: true, element: <Navigate to="config" replace /> },
-              { path: "config", element: <PuzzleConfig /> },
-              { path: "play", element: <PuzzleScreen /> },
+              { path: 'config', element: <PuzzleConfig /> },
+              { path: 'play', element: <PuzzleScreen /> },
             ],
           },
         ],
       },
     ],
   },
-  { path: "*", element: <NotFound /> },
+  { path: '*', element: <NotFound /> },
 ]);
 
 export default function Router() {
