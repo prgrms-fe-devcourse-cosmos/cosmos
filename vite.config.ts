992c93bb61678b0ab18f785b1edb4c782f57fc55
TYss00 <rlaxodus203@naver.com>
2025-06-09 12:50:18 +0900

    모바일테스트용 서버 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 모바일 테스트용 서버
+  server: {
+    host: true,
+  },
   plugins: [react(), tailwindcss()],
 });
