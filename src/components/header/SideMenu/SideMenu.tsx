55d79f64bc372372b6250c54d022f5c3b3fda797
hxezu <hailey_apple@icloud.com>
2025-06-09 22:05:12 +0900

    ✨ sidemenu 아이템 추가

--- src/components/header/SideMenu/SideMenu.tsx
+++ src/components/header/SideMenu/SideMenu.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Menu, X } from "lucide-react";
+import { Armchair, Gamepad, Menu, Telescope, X } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function SideMenu() {
   const [open, setOpen] = useState(false);
   const toggleMenu = () => setOpen((prev) => !prev);
+  const navigate = useNavigate();
 
   return (
     <>
@@ -22,6 +24,30 @@ export default function SideMenu() {
               strokeWidth={1}
               className="cursor-pointer absolute top-5 right-10 "
             />
+
+            <div className="flex flex-col h-full px-5 pt-20">
+              <button
+                onClick={() => navigate("/daily")}
+                className="text-left flex gap-4 items-center cursor-pointer hover:text-[color:var(--primary-300)] py-2"
+              >
+                <Telescope className="w-[18px]" />
+                <p className="text-sm pt-1">Daily Space</p>
+              </button>
+              <button
+                onClick={() => navigate("/lounge")}
+                className="text-left flex gap-4 items-center cursor-pointer hover:text-[color:var(--primary-300)] py-2"
+              >
+                <Armchair className="w-[18px]" />
+                <p className="text-sm pt-1">Lounge</p>
+              </button>
+              <button
+                onClick={() => navigate("/lab")}
+                className="text-left flex gap-4 items-center cursor-pointer hover:text-[color:var(--primary-300)] py-2"
+              >
+                <Gamepad className="w-[18px]" />
+                <p className="text-sm pt-1">Cosmo Lab</p>
+              </button>
+            </div>
           </div>
         </aside>
       )}
