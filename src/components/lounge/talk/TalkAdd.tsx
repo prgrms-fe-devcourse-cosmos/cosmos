80cd3d5fa311649b280967b62a1d3508db10b761
jieun22222 <wldmszz61@gmail.com>
2025-06-12 16:11:18 +0900

    ✨ Talk 게시글 등록 구현 중

--- src/components/lounge/talk/TalkAdd.tsx
+++ src/components/lounge/talk/TalkAdd.tsx
@@ -1,12 +1,57 @@
 import { ArrowLeft } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "../../common/Button";
+import { useEffect, useState } from "react";
+import supabase from "../../../utils/supabase";
+import { addTalkPost } from "../../../api/talk/talk";
 
 export default function TalkAdd() {
   const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [profileId, setProfileId] = useState<string | null>(null);
+
+  // cancel 버튼 클릭 시 폼에 입력된 내용들 초기화
+  const reset = () => {
+    setTitle("");
+    setContent("");
+  };
+
+  // 로그인한 유저 profile_id 가져오기
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data, error } = await supabase.auth.getUser();
+      if (error) {
+        console.error("사용자 정보를 가져오는 데 실패:", error);
+        return;
+      }
+      setProfileId(data.user?.id ?? null);
+    };
+
+    fetchUser();
+  }, []);
+
+  // 게시글 등록
+  const handleSave = async () => {
+    if (!profileId) {
+      alert("로그인이 필요합니다.");
+      return;
+    }
+
+    try {
+      await addTalkPost(title, content, profileId);
+      navigate("/lounge/talk"); // 작성 후 게시판으로 이동
+    } catch (err) {
+      console.error("게시글 등록 실패:", err);
+      alert("게시글 등록에 실패했습니다.");
+    }
+  };
+
+  const isEmpty = title.trim() !== "" && content.trim() !== "";
+
   return (
     <div className="px-8 py-6 bg-[#141414]/80 rounded-[8px]">
-      <div className="wrapper">
+      <form className="wrapper">
         {/* 뒤로가기 */}
         <div className="mb-8">
           <button
@@ -22,6 +67,8 @@ export default function TalkAdd() {
         <div className="mb-8">
           <p className="mb-2">제목</p>
           <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             type="text"
             placeholder="제목을 작성해주세요"
             className="border border-[#909090] focus:outline-none w-full px-6 py-[12px] rounded-[8px]"
@@ -31,16 +78,25 @@ export default function TalkAdd() {
         <div className="mb-8">
           <p className="mb-2">본문</p>
           <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
             placeholder="내용을 작성해주세요"
             className="w-full h-[390px] resize-none border border-[#909090] focus:outline-none focus:border-[] px-6 py-[12px] rounded-[8px]"
           />
         </div>
         {/* 취소, 저장버튼 */}
         <div className="flex justify-center gap-6">
-          <Button variant="dark_line">CANCEL</Button>
-          <Button variant="disabled">SAVE</Button>
+          <Button variant="dark_line" onClick={reset}>
+            CANCEL
+          </Button>
+          <Button
+            variant={isEmpty ? "neon_filled" : "disabled"}
+            onClick={handleSave}
+          >
+            SAVE
+          </Button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
--- src/components/lounge/talk/TalkCard.tsx
+++ src/components/lounge/talk/TalkCard.tsx
@@ -1,46 +1,50 @@
 import { MessageSquare, Heart } from "lucide-react";
 import { TalkPost } from "../../../types/talk";
+import profileImage from "../../../assets/images/profile.svg";
+import { NavLink } from "react-router-dom";
 
 export default function TalkCard({ post }: { post: TalkPost }) {
   return (
-    <div className="px-9 py-8 border-l border-l-[#7A9100] mb-6 bg-[#141414]/80">
-      <div className="wrapper">
-        {/* 유저 정보, 게시글 등록 시간 */}
-        <div className="flex gap-[22px] items-center">
-          <img
-            src={post.profiles.avatar_url ?? "../../images/profile.svg"}
-            alt="유저프로필"
-            className="w-[40px] h-[40px] rounded-full object-cover"
-          />
-          {/* <div className="w-[40px] h-[40px] bg-amber-900 rounded-full"></div> */}
-          <div>
-            <h3 className="font-semibold">{post.profiles.username}</h3>
-            <p className="text-[#696969] font-light text-sm">
-              {new Date(post.created_at).toLocaleString()}
-            </p>
+    <NavLink to={`${post.id}`} key={post.id}>
+      <div className="px-9 py-8 border-l border-l-[#7A9100] mb-6 bg-[#141414]/80">
+        <div className="wrapper">
+          {/* 유저 정보, 게시글 등록 시간 */}
+          <div className="flex gap-[22px] items-center">
+            <img
+              src={post.profiles.avatar_url || profileImage}
+              alt="유저프로필"
+              className="w-[40px] h-[40px] rounded-full object-cover"
+            />
+            {/* <div className="w-[40px] h-[40px] bg-amber-900 rounded-full"></div> */}
+            <div>
+              <h3 className="font-semibold">{post.profiles.username}</h3>
+              <p className="text-[#696969] font-light text-sm">
+                {new Date(post.created_at).toLocaleString()}
+              </p>
+            </div>
           </div>
-        </div>
-        {/* 게시글 제목 */}
-        <h3 className="my-6 font-medium">{post.title}</h3>
-        {/* 게시글 내용 + 댓글수 + 좋아요수 */}
-        <div className="flex justify-between items-end">
-          {/* 게시글 내용 */}
-          <p className="font-light text-sm">{post.content}</p>
-          {/* 댓글수 + 좋아요수 */}
-          <div className="flex gap-4">
-            {/* 댓글수 */}
-            <p className="inline-flex items-center">
-              <MessageSquare size={16} />
-              <span className="ml-2 text-[13px]">10</span>
-            </p>
-            {/* 좋아요수 */}
-            <p className="inline-flex items-center">
-              <Heart size={15} className="text-[#D0F700]" />
-              <span className="ml-2 text-[13px]">{post.like_count}</span>
-            </p>
+          {/* 게시글 제목 */}
+          <h3 className="my-6 font-medium">{post.title}</h3>
+          {/* 게시글 내용 + 댓글수 + 좋아요수 */}
+          <div className="flex justify-between items-end">
+            {/* 게시글 내용 */}
+            <p className="font-light text-sm">{post.content}</p>
+            {/* 댓글수 + 좋아요수 */}
+            <div className="flex gap-4">
+              {/* 댓글수 */}
+              <p className="inline-flex items-center">
+                <MessageSquare size={16} />
+                <span className="ml-2 text-[13px]">10</span>
+              </p>
+              {/* 좋아요수 */}
+              <p className="inline-flex items-center">
+                <Heart size={15} className="text-[#D0F700]" />
+                <span className="ml-2 text-[13px]">{post.like_count}</span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </NavLink>
   );
 }
