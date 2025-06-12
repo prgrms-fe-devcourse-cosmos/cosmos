fef3be1da7e87a32ff6314eb804e2a6dc873a8c4
jieun22222 <wldmszz61@gmail.com>
2025-06-12 14:20:55 +0900

    🎨 Talk 퍼블리싱 완료

---
+++ src/components/lounge/LoungeComment.tsx
@@ -0,0 +1,32 @@
+import Button from "../common/Button";
+import LoungeCommentList from "./LoungeCommentList";
+
+export default function LoungeComment() {
+  return (
+    <div>
+      <div className="wrapper">
+        {/* 댓글 카운트 */}
+        <h3 className="mb-7 text-[#D0F700] font-medium">COMMENTS (3)</h3>
+
+        {/* 댓글 리스트 */}
+        <LoungeCommentList />
+
+        {/* 댓글 입력창 */}
+        <div className="w-full relative">
+          <input
+            placeholder="댓글을 입력하세요"
+            type="text"
+            className={`w-full pl-4 sm:pl-[24px] h-[49px] md:h-[51px] 
+                  border rounded-[8px] focus:outline-none`}
+          />
+          <Button
+            variant="disabled"
+            className="h-full border-[#D0F700] absolute right-0 top-0 rounded-tl-none rounded-bl-none"
+          >
+            ENTER
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/lounge/talk/TalkDetail.tsx
+++ src/components/lounge/talk/TalkDetail.tsx
@@ -1,11 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../../common/Button";
+import LoungeComment from "../LoungeComment";
+import { ArrowLeft } from "lucide-react";
 
 export default function TalkDetail() {
+  const navigate = useNavigate();
   return (
-    <div className="px-8 py-6 border border-amber-600 bg-[#141414]/80">
+    <div className="px-8 py-6 bg-[#141414]/80">
       <div className="wrapper">
         {/* 뒤로가기버튼 */}
-
+        <div className="mb-8">
+          <button
+            className="font-yapari text-[#D0F700] py-4 cursor-pointer flex items-center gap-2 text-[14px]"
+            onClick={() => navigate(-1)}
+          >
+            <ArrowLeft className="w-4 h-4 text-[#D0F700] cursor-pointer" /> BACK
+          </button>
+        </div>
         {/* 유저정보 + 게시글 등록 날짜 + 팔로우버튼/메뉴버튼 */}
         <div className="flex justify-between items-start">
           <div className="flex gap-[22px] items-center">
@@ -26,10 +37,12 @@ export default function TalkDetail() {
         {/* 게시글 */}
         <section>
           {/* 제목 */}
-          <h3>입문하고 싶어서 기웃기웃</h3>
+          <h3 className="mt-7 mb-8 font-medium text-[20px]">
+            입문하고 싶어서 기웃기웃
+          </h3>
 
           {/* 내용 */}
-          <div>
+          <div className="mb-8">
             <p>
               끓는 공자는 것이다 뿐이다 우리 바이며 군영과 어디 들어 스며들어
               크고 때에 청춘 천고에 기관같이 그리하였는가 일월과 따뜻한 얼음
@@ -42,6 +55,9 @@ export default function TalkDetail() {
               무엇이 그것은 방황하여도.
             </p>
           </div>
+
+          {/* 댓글 */}
+          <LoungeComment />
         </section>
       </div>
     </div>
