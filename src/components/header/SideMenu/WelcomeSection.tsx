9475a833ae28c05cad2ceba7f25746e594293f7a
hxezu <hailey_apple@icloud.com>
2025-06-13 09:13:57 +0900

    💄 welcome 문구 색상 변경

--- src/components/header/SideMenu/WelcomeSection.tsx
+++ src/components/header/SideMenu/WelcomeSection.tsx
@@ -6,7 +6,7 @@ export default function WelcomeSection() {
     <div className="text-2xl space-y-2">
       <img src={logo} className="size-12" />
       <div className="space-y-1">
-        <p>WELCOME</p> <p>COSMOS</p>
+        <p className="text-[color:var(--primary-300)]">WELCOME</p> <p>COSMOS</p>
       </div>
     </div>
   );
