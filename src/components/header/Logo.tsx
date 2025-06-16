66f13840afe4e8f712dc09f6d743fd5c0fef7f1f
WON <won431236@gmail.com>
2025-06-16 22:05:35 +0900

    일부 수정

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import logo from "../../assets/images/default-logo.svg";
 import greenLogo from "../../assets/images/green-logo.svg";
