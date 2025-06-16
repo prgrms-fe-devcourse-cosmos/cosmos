f91dc6e627f9ca3f169a895e850df56193502dda
hxezu <hailey_apple@icloud.com>
2025-06-17 00:25:08 +0900

    💄 Contact 페이지 퍼블리싱 완료

--- src/routes/pages/Contact.tsx
+++ src/routes/pages/Contact.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import Button from "../../components/common/Button";
 
 export default function Contact() {
-  return <div>Content</div>;
+  return (
+    <div className="w-full flex flex-col justify-center items-center text-white">
+      <p className="text-3xl font-[yapari] uppercase">Our stella crew</p>
+      <DotLottieReact
+        src="https://lottie.host/28721b8c-299c-44bb-86eb-6ff6f595d0c2/yQoOd7yeu3.lottie"
+        loop
+        autoplay
+        style={{ width: "400px", height: "200px" }}
+      />
+      <a
+        href="https://github.com/prgrms-fe-devcourse-cosmos/cosmos.git"
+        className="group font-[yapari] text-[color:var(--primary-300)] my-7  cursor-pointer"
+      >
+        <Button variant="hover_fill">COSMOS REPO</Button>
+      </a>
+      <div className="group"></div>
+      <div className="mt-4 text-lg flex gap-10 font-[watermelonSalad]">
+        <a href="https://github.com/jieun22222">FE 송지은</a>
+        <a href="https://github.com/keemeunji">FE 김은지</a>
+        <a href="https://github.com/TYss00">FE 김태연</a>
+        <a href="https://github.com/won431236">FE 최원일</a>
+        <a href="https://github.com/hxezu">FE 현혜주</a>
+      </div>
+    </div>
+  );
 }
