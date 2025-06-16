98db12ce2ed56ea43f0736ff3fd31e88b1fec28d
hxezu <hailey_apple@icloud.com>
2025-06-16 17:41:54 +0900

    ♻️ rootlayout 내 로딩 상태 관리 제거 (퍼즐 로딩 버그 이슈 문제로 제거)

--- src/routes/pages/lab/puzzle/PuzzleScreen.tsx
+++ src/routes/pages/lab/puzzle/PuzzleScreen.tsx
@@ -65,7 +65,11 @@ export default function PuzzleScreen() {
   };
 
   if (!config || !imageUrl || !imageLoaded || isLoading) {
-    return <LoadingSpinner />;
+    return (
+      <div className="w-full h-full flex justify-center items-center">
+        <LoadingSpinner />
+      </div>
+    );
   }
   return (
     <>
