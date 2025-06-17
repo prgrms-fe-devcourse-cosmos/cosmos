cd30351805c3b780fd8dede198c178e2d692daa8
hxezu <hailey_apple@icloud.com>
2025-06-18 03:39:43 +0900

    💄 홈화면 반응형 스타일링 적용

--- src/components/home/ExploreNav.tsx
+++ src/components/home/ExploreNav.tsx
@@ -10,7 +10,7 @@ export default function ExploreNav() {
   return (
     <div className="flex flex-col gap-2">
       <span className="text-lg font-medium">Explore</span>
-      <div className="w-60 flex flex-col items-start text-[color:var(--gray-200)] gap-1">
+      <div className="w-40 lg:w-60 flex flex-col items-start text-[color:var(--gray-200)] gap-1">
         {explorePageList.map((page) => (
           <PageListItem key={page.name} name={page.name} route={page.route} />
         ))}
