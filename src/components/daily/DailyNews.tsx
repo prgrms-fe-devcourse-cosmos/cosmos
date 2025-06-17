708924ee1f6765c8a4051c570b18a4e6521ce94a
hxezu <hailey_apple@icloud.com>
2025-06-17 10:21:32 +0900

    💄 디자인 QA 작업 완료

--- src/components/daily/DailyNews.tsx
+++ src/components/daily/DailyNews.tsx
@@ -9,18 +9,18 @@ export default function DailyNews() {
 
   return (
     <div>
-      <h1 className="text-center font-[yapari] text-[var(--primary-300)] text-2xl mb-10 sm:mb-17">
+      <h1 className="text-center font-[yapari] text-[var(--primary-300)] text-lg md:text-2xl mb-10 sm:mb-17">
         Space News
       </h1>
       <div
-        className="w-[236px] sm:w-[680px] md:w-[880px] lg:w-[1080px]
+        className="w-[270px] sm:w-[540px] md:w-[640px] lg:w-[920px] xl:w-[1080px]
           h-[247px] md:h-[370px]"
       >
         <Swiper
           breakpoints={{
             0: { slidesPerView: 1 },
-            640: { spaceBetween: 20, slidesPerView: 3 },
-            768: { spaceBetween: 60, slidesPerView: 3 },
+            640: { spaceBetween: 20, slidesPerView: 2 },
+            768: { spaceBetween: 40, slidesPerView: 2 },
             1024: { spaceBetween: 80, slidesPerView: 3 },
           }}
         >
